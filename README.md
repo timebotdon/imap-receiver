d86d8aff77f08d2f73eb430ca40b16f85868b05c
Donovan Choy <choyyld@gmail.com>
2020-10-05 01:34:36 +0800

    update app.py, README

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
-## Libs Required
-* os
-* email
-* imaplib
-* html2text
+OpenCTI import tool to extract and parse json data forom email attachments.
 
-Installation of `html2text` is required.
+## Installation
+Installation of 3rd party modules is required.
 * `pip install html2text`
+* `pip install mailparser`
+* `pip install pycti`
+
+## Libs required
+* mailparser
+* imaplib
+* json
+* pycti
 
 ## Testing
 * Update `USER` and `PASS` with gmail credentials.
+* Update `api_url` and `api_token` with opencti platform URL/Token
 * Run: `python app.py`
--- app.py
+++ app.py
@@ -1,27 +1,29 @@
 import mailparser
 import imaplib
 import json
+import base64
+import re
 from pycti import OpenCTIApiClient
 
 # OpenCTI connector credentials
 api_url = ''
 api_token = ''
 
-# Email credentials
-USER = ''
-PASS = ''
+# Email configuration
+email_imapserver = 'imap.gmail.com' # Imap server address/domain name
+email_folder = 'test' # Target folder to ingest mail from
+email_username = ''
+email_password = ''
 
 
 def importToOCTI(jsonData):
   # OpenCTI API client initialization
   opencti_api_client = OpenCTIApiClient(api_url, api_token)
   # Import the bundle
-  opencti_api_client.stix2.import_bundle_from_json(jsonData, update=False)
-
+  opencti_api_client.stix2.import_bundle_from_json(jsonData, update=True)
 
 def getMessageText():
   for i in range(1, total_messages+1, 1):
-    #print("msg number:", i)
 
     # fetch the email message by ID
     res, msg = imap.fetch(str(i), "RFC822")
@@ -31,41 +33,32 @@ def getMessageText():
       if isinstance(response, tuple):
         # parse a bytes email into a message object
         mail = mailparser.parse_from_bytes(response[1])
-        msgObject = mail.message
-        #print(content_type)
+        msgAttachContentType = mail.attachments[0]["mail_content_type"]
+        attachmentFilename = mail.attachments[0]["filename"]
+        
+        filenameConventionReg = 'SINGCERT_\d\d-\d\d-\d\d.json' #change naming convention regex here
 
-        if msgObject.is_multipart():
-          # Walk through each part
-          for part in msgObject.walk():
-            #Get content type
-            content_type = part.get_content_type()
-            content_disposition = str(part.get("Content-Disposition"))
-            #print(content_disposition)
-
-            if content_type == "application/json":
-              try:
-                attachContent = part.get_payload(decode = True).decode()
-                #print(attachContent)
-                jsonData = json.loads(attachContent)
-                #print(jsonData)
-                importToOCTI(attachContent)
-                #importToOcti(attachContent) <----- send json data to OCTI here
-              except Exception as e:
-                print(e)
+        # If a json file is found as an attachment and matches naming convention, decode content and upload to opencti.
+        if msgAttachContentType == "application/json" and (re.search(filenameConventionReg, attachmentFilename)):
+          try:
+            msgAttachPayload = base64.b64decode(mail.attachments[0]["payload"]).decode('utf-8')
+            importToOCTI(msgAttachPayload)
+          except Exception as error:
+            print(error)
 
 
 # create an IMAP4 class with SSL 
-imap = imaplib.IMAP4_SSL("imap.gmail.com")
+imap = imaplib.IMAP4_SSL(email_imapserver)
 
 # authenticate
-imap.login(USER, PASS)
+imap.login(email_username, email_password)
 
 # Select mail folder
-status, messages = imap.select("test")
+status, messages = imap.select(email_folder)
 
 # Get total number of emails
 total_messages = int(messages[0])
-#print("Total Mails in INBOX:", total_messages)
+print("Total Mails in folder:", total_messages)
 
 # get message content
 getMessageText()
